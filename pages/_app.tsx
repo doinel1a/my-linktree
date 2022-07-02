7b205bc930dacb6d4e8b78896b533809bd889852
doinel1a <doinel1atanasiu@gmail.com>
2022-07-02 18:56:15 +0200

    Home & 404 page implemented, Metatags and Manifest added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app';
 
+import { ContextProvider } from '../contexts/ContextProvider';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return(
-        <Component {...pageProps} />
+        <ContextProvider>
+            <div className='w-screen min-h-screen flex flex-col items-center justify-center'>
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+            </div>
+        </ContextProvider>
     );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,65 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import Header from '../components/Header';
+import { useStateContext } from '../contexts/ContextProvider';
+
 import Main from '../components/Main';
-import Footer from '../components/Footer';
 
 const Home: NextPage = () => {
+    const { isDarkMode } = useStateContext();
+    
     return(
-        <div className='w-screen min-h-screen flex flex-col items-center justify-center'>
+        <>
             <Head>
                 <title>Doinel Atanasiu — Quick Links</title>
 
-                {/* Generics */}
+                <meta
+                    name="description"
+                    content="Doinel Atanasiu's Quick Links"
+                />
+                <meta name="keywords" content="Quick Links" />
+                <meta name="author" content="Doinel Atanasiu" />
+                <meta name="copyright" content="Doinel Atanasiu" />
+                <meta name="robots" content="index, follow" />
+                <meta name="rating" content="general" />
+
+                {/* Open Graph */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@SITE" />
+                <meta name="twitter:creator" content="@AtanasiuDoinel" />
+                <meta name="twitter:title" content="Doinel Atanasiu — Quick Links" />
+                <meta
+                    name="twitter:description"
+                    content="Doinel Atanasiu's Quick Links"
+                />
+                <meta
+                    name="twitter:image"
+                    content="IMAGE URL"
+                />
+                <meta property="og:url" content="URL" />
+                <meta property="og:title" content="Doinel Atanasiu — Quick Links" />
+                <meta
+                    property="og:description"
+                    content="Doinel Atanasiu's Quick Links"
+                />
+                <meta
+                    property="og:image"
+                    content="IMAGE URL"
+                />
+
+                {/* Manifest */}
+                <link rel="manifest" href="/manifest.json"></link>
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="application-name" content="D1A — Quick Links" />
+                <meta name="apple-mobile-web-app-title" content="D1A — Quick Links" />
+                <meta name="theme-color" content="#0063EB" />
+                <meta name="msapplication-navbutton-color" content="#0063EB" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <meta name="msapplication-starturl" content="/" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+                {/* Favicon - Generics */}
                 <link rel='icon' href='/favicon/favicon-32.png' sizes='32x32' />
                 <link rel='icon' href='/favicon/favicon-57.png' sizes='57x57' />
                 <link rel='icon' href='/favicon/favicon-76.png' sizes='76x76' />
@@ -20,19 +68,18 @@ const Home: NextPage = () => {
                 <link rel='icon' href='/favicon/favicon-192.png' sizes='192x192' />
                 <link rel='icon' href='/favicon/favicon-228.png' sizes='228x228' />
 
-                {/* Android */}
+                {/* Favicon - Android */}
                 <link rel='shortcut icon' sizes='196x196' href='/favicon/favicon-196.png' />
 
-                {/* iOS */}
+                {/* Favicon - iOS */}
                 <link rel='apple-touch-icon' href='/favicon/favicon-120.png' sizes='120x120' />
                 <link rel='apple-touch-icon' href='/favicon/favicon-152.png' sizes='152x152' />
                 <link rel='apple-touch-icon' href='/favicon/favicon-180.png' sizes='180x180' />
             </Head>
-
-            <Header />
-            <Main />
-            <Footer />
-        </div>
+            <div className={` w-full h-full flex flex-1 justify-center items-center transition-colors overflow-hidden ${ isDarkMode ? 'bg-neutral-700' : 'bg-slate-100' } `}>
+                <Main />
+            </div>
+        </>
     );
 };
 
